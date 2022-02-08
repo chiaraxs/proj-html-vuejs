25344073be5fa4353fac09e8e82822ea71d0ec05
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-08 19:27:22 +0100

    Completed: BestSeller

--- src/components/BestSeller.vue
+++ src/components/BestSeller.vue
@@ -1,5 +1,75 @@
 <template>
-    <div></div>
+    <div class="container-fluid">
+        <div class="container mt-5">
+            
+            <div class="text-center">
+                <h1>Best Seller</h1>
+                <p class="text-muted">Must have products from our top sellers</p>
+            </div>
+                    
+                    
+            <!-- OUTFITS CONTAINER -->
+            <div class="row mt-5">
+
+                <!-- OUTFIT 1 -->
+                <div class="col">
+                    
+                    <div class="container-outfit">
+                        <img src="../assets/img/spring_printed_dress-400x520.jpg" alt="">
+                    </div>
+                   
+                        
+                </div>
+                <!-- / OUTFIT 1  -->
+
+                <!-- OUTFIT 2 -->
+                <div class="col">
+                    
+                    <div class="container-outfit">
+                        <img src="../assets/img/modern_love_tee-400x520.jpg" alt="">
+                    </div>
+                    
+                        
+                </div>
+                <!-- / OUTFIT 2 -->
+                        
+                <!-- OUTFIT 3 -->
+                <div class="col">
+
+                    <div class="container-outfit">
+                        <img src="../assets/img/black_leather_jacket-400x520.jpg" alt="">
+                    </div>
+                    
+                </div>
+                <!-- / OUTFIT 3 -->
+                            
+                <!-- OUTFIT 4 -->
+                <div class="col">
+                    
+                    <div class="container-outfit">
+                        <img src="../assets/img/black_elegant_leather_jacket-400x520.jpg" alt="">
+                    </div>
+                    
+                </div>
+                <!-- / OUTFIT 4 -->
+
+                 <!-- OUTFIT 5 -->
+                <div class="col">
+                    
+                    <div class="container-outfit">
+                        <img src="../assets/img/hipster_black_top-400x520.jpg" alt="">
+                    </div>
+                    
+                </div>
+                <!-- / OUTFIT 5 -->
+
+                        
+            </div>
+            <!-- / OUTFITS CONTAINER  -->
+
+            
+        </div>
+    </div>
 </template>
 
 
@@ -12,4 +82,9 @@ export default {
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
 
+.container-outfit{
+    img{
+        width: 200px;
+    }
+}
 </style>
