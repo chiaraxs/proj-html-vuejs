089c9fce02c461af96c18744b0ec2efa74f0986a
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-08 17:24:17 +0100

    Completed: HeroBox

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -4,33 +4,36 @@
     <div class="container-header-box">
 
         <!-- PRE HEADHER - COMPONENTE A PARTE? -->
-        <div class="container-fluid container-pre-header d-flex">
-            <div class="container d-flex ps-5 pt-3">
+        <div class="container-fluid">
+            <div class="container container-pre-header d-flex">
+                <div class="d-flex ps-5 pt-3">
+                    
+                    <i class="fab fa-facebook-f mx-2"></i>
+                    <i class="fab fa-twitter mx-2"></i>
+                    <i class="fab fa-instagram mx-2"></i>
+                    <i class="fab fa-youtube mx-2"></i>
                 
-                <i class="fab fa-facebook-f mx-2"></i>
-                <i class="fab fa-twitter mx-2"></i>
-                <i class="fab fa-instagram mx-2"></i>
-                <i class="fab fa-youtube mx-2"></i>
-            
-            </div>
-            
-            <div class="container d-flex justify-content-end">
-                <div class="shopping"> 
-                    <p class="px-2 py-2">Shooping Cart</p>  
                 </div>
-               <div class="my-account"> 
-                   <p class="px-2 py-2">My Account <i class="fas fa-chevron-down"></i></p> 
+                
+                <div class="container d-flex justify-content-end text-center">
+                    <div class="shopping"> 
+                        <p class="px-4 py-2">Shooping Cart</p>  
+                    </div>
+                <div class="my-account"> 
+                    <p class="px-4 py-2">My Account <i class="fas fa-chevron-down"></i></p> 
+                    </div>
+                <div class="cart"> 
+                    <p class="px-4 py-2 text-uppercase"><i class="fas fa-shopping-cart px-1 py-1 me-2"></i>Cart <i class="fas fa-chevron-down"></i></p> 
+                    </div>
                 </div>
-               <div class="cart"> 
-                   <p class="px-2 py-2 text-uppercase"><i class="fas fa-shopping-cart px-1 py-1 me-2"></i>Cart <i class="fas fa-chevron-down"></i></p> 
-                </div>
+                
             </div>
             
         </div>
         <!-- / PRE HEADHER -->
         
         <!-- NAVBAR -->
-        <nav class="navbar container-fluid navbar-light bg-light d-flex justify-content-between">
+        <nav class="container d-flex justify-content-between mt-3">
 
             <!-- LOGO -->
             <div class="logo-container ms-5 py-2">
@@ -39,7 +42,7 @@
             <!-- / LOGO -->
                 
             <!-- CONTAINER MENU-->
-            <div class="container container-menu d-flex justify-content-center">
+            <div class="container-menu">
                    
                 <!-- MENU LIST -->
                 <ul class="d-flex list-unstyled ">
@@ -53,7 +56,7 @@
             <!-- / CONTAINER MENU  -->
                 
             <!-- CONTAINER BUTTON/ICON -->
-            <div class="d-flex justify-content-end">
+            <div class="container-button-icon">
                 <!-- BUTTON -->
                 <button type="button" class="btn text-light text-uppercase rounded-pill mb-3 me-4 ">Shop now!</button>
                 <!-- / BUTTON -->
@@ -87,11 +90,16 @@ export default {
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
 
-.container-pre-header{
+.container-fluid{
     border-bottom: 1px solid gray;
-
-    .shopping, .my-account{
+    
+ .container-pre-header{
+     .shopping, .my-account{
         border-right: 1px solid gray;
+
+        p{
+         color: $text-primary-color;
+        }
     }
 
     .fab, .fas{
@@ -101,21 +109,33 @@ export default {
     .fa-chevron-down{
         font-size: 10px;
     }
+
+    .cart{
+        width: 200px;
+        background-color: rgb(241, 241, 241);
+    }
+
+ } 
+
+    
 }
 
 a{
-    color: gray;
+    color: $text-primary-color;
 }
 .fa-chevron-down{
     font-size: 10px;
 }
 
 .fa-search{
-    font-size: 25px;
+    font-size: 20px;
 }
 
 button{
     background-color: $button-color;
+        &:hover{
+                background-color: $button-hover;
+            }
 }
 
 
--- src/components/HeroBox.vue
+++ src/components/HeroBox.vue
@@ -1,5 +1,24 @@
 <template>
-    <div></div>
+    
+    
+    <div class="hero container-fluid mt-4">
+        <div class="container">
+            <div class="row text-center container-info">
+                <h1>Brand New Arrivals</h1>
+                <p class="text-uppercase">new collection from new york</p>
+
+                <div class="button py-3">
+                    <button type="button" class="btn text-light text-uppercase rounded-pill border border-white mx-3">view all</button>
+                    <button type="button" class="btn text-light text-uppercase rounded-pill border border-white">lookbook</button>
+                </div>
+            </div>
+            
+        </div>
+
+    </div>
+
+
+
 </template>
 
 
@@ -11,5 +30,32 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
+
+.hero{
+    background-image: $img-hero;
+    height: 500px;
+    background-size: cover;
+    background-position: bottom bottom;
+    color: $text-color-secondary;
+    
+
+    .container-info{
+        padding-top: 150px;
+
+        h1{
+            font-size: 80px;
+        }
+
+        p{
+            font-size: 20px;
+        }
+
+       button{
+            &:hover{
+                background-color: $button-hover;
+            }
+        } 
+    }
+}
 
 </style>
