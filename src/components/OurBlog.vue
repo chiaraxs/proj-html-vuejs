f6175909d8e397db01629d9d1783a2d2b6d26c00
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-09 11:50:44 +0100

    Completed: OurBlog

--- src/components/OurBlog.vue
+++ src/components/OurBlog.vue
@@ -1,5 +1,64 @@
 <template>
-    <div></div>
+    
+    <div class="container-fluid container-shipping mt-5">
+        <div class="container d-flex justify-content-center">
+            <div class="row mt-3">
+            
+                <div class="text-center mb-4">
+                    <h1>From Our Blog</h1>
+                    <p class="text-muted">The latest Classic Shop news</p>
+                </div>
+
+                <!-- BOX 1 -->
+                <div class="col-4">
+                    <div class="box">
+                        <div>
+                            <img src="../assets/img/post_img_12-320x202.jpg" alt="">
+                        </div>
+                        <span class="fw-bold">Aenean lobortis sapien enim viverra</span>
+                        <br>
+                        <span class="text-muted">September 9th, 2015 | 0 comments</span>
+                        <br>
+                        <span class="text-muted">Etiam finibus suscipit odio, quis cursus dui pharetra vitae. Donec hendrerit tincidunt congue.</span>
+                    </div>
+                </div>
+                <!-- / BOX 1 -->
+
+                <!-- BOX 2 -->
+                <div class="col-4 ">
+                    <div class="box">
+                        <div>
+                            <img src="../assets/img/post_img_11-320x202.jpg" alt="">
+                        </div>
+                        <span class="fw-bold">Duic ac massa semper maximus</span>
+                        <br>
+                        <span class="text-muted">September 9th, 2015 | 0 comments</span>
+                        <br>
+                        <span class="text-muted">Etiam finibus suscipit odio, quis cursus dui pharetra vitae. Donec hendrerit tincidunt congue.</span>
+                    </div>
+                </div>
+                <!-- / BOX 2 -->
+
+                <!-- BOX 3 -->
+                <div class="col-4 ">
+                    <div class="box">
+                        <div>
+                            <img src="../assets/img/post_img_10-320x202.jpg" alt="">
+                        </div>
+                        <span class="fw-bold">Nunc femrint nulla eu justo sem id</span>
+                        <br>
+                        <span class="text-muted">September 9th, 2015 | 0 comments</span>
+                        <br>
+                        <span class="text-muted">Etiam finibus suscipit odio, quis cursus dui pharetra vitae. Donec hendrerit tincidunt congue.</span>
+                    </div>
+                </div>
+                
+                <!-- / BOX 3 -->
+            </div>
+        </div>
+        
+    </div>
+
 </template>
 
 
@@ -11,5 +70,10 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
+
+.box{
+    width: 300px;
+    line-height: 30px;
+}
 
 </style>
