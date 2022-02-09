f7887f9ea43a356a8bc788a3e6471a61be75b5ff
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-09 10:56:56 +0100

    Completed: NewArrivals

--- src/components/NewArrivals.vue
+++ src/components/NewArrivals.vue
@@ -1,5 +1,75 @@
 <template>
-    <div></div>
+    <div class="container-fluid">
+        <div class="container mt-5">
+            
+            <div class="text-center">
+                <h1>New Arrivals</h1>
+                <p class="text-muted">Brand new products from top designers</p>
+            </div>
+                    
+                    
+            <!-- OUTFITS CONTAINER -->
+            <div class="row row-cols-5 mt-5">
+
+                <!-- OUTFIT 1 -->
+                <div class="col">
+                    
+                    <div class="container-outfit">
+                        <img class="img-fluid" src="../assets/img/black_elegant_leather_jacket-400x520.jpg" alt="">
+                    </div>
+                   
+                        
+                </div>
+                <!-- / OUTFIT 1  -->
+
+                <!-- OUTFIT 2 -->
+                <div class="col">
+                    
+                    <div class="container-outfit">
+                        <img class="img-fluid" src="../assets/img/hipster_black_top-400x520.jpg" alt="">
+                    </div>
+                    
+                        
+                </div>
+                <!-- / OUTFIT 2 -->
+                        
+                <!-- OUTFIT 3 -->
+                <div class="col">
+
+                    <div class="container-outfit">
+                        <img class="img-fluid" src="../assets/img/black_leather_suit-400x520.jpg" alt="">
+                    </div>
+                    
+                </div>
+                <!-- / OUTFIT 3 -->
+                            
+                <!-- OUTFIT 4 -->
+                <div class="col">
+                    
+                    <div class="container-outfit">
+                        <img class="img-fluid" src="../assets/img/spring_printed_dress-400x520.jpg" alt="">
+                    </div>
+                    
+                </div>
+                <!-- / OUTFIT 4 -->
+
+                 <!-- OUTFIT 5 -->
+                <div class="col">
+                    
+                    <div class="container-outfit">
+                        <img class="img-fluid" src="../assets/img/modern_love_tee-400x520.jpg" alt="">
+                    </div>
+                    
+                </div>
+                <!-- / OUTFIT 5 -->
+
+                        
+            </div>
+            <!-- / OUTFITS CONTAINER  -->
+
+            
+        </div>
+    </div>
 </template>
 
 
