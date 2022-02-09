081c2a963d12984b146692078fd06b7ffa85db4c
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-09 13:32:26 +0100

    Completed: BrandLogos

--- src/components/BrandLogs.vue
+++ src/components/BrandLogs.vue
@@ -1,5 +1,55 @@
 <template>
-    <div></div>
+    <div class="container-logo container-fluid mt-5">
+        <div class="container">
+
+            <h5 class="text-center text-muted pt-5">BRAND LOGOS</h5>
+            
+            <div class="row row-cols-5 mt-5">
+
+                <!-- LOGO 1 -->
+                <div class="col">
+                    <div class="logo-1">
+                        <img src="../assets/img/b_logotype_6.png" alt="">
+                    </div>
+                </div>
+                <!-- / LOGO 1 -->
+
+                <!-- LOGO 2 -->
+                <div class="col">
+                    <div class="logo-1">
+                        <img src="../assets/img/b_logotype_7.png" alt="">
+                    </div>
+                </div>
+                <!-- / LOGO 2 -->
+
+                <!-- LOGO 3 -->
+                <div class="col">
+                    <div class="logo-1">
+                        <img src="../assets/img/b_logotype_1.png" alt="">
+                    </div>
+                </div>
+                <!-- / LOGO 3 -->
+
+                <!-- LOGO 4 -->
+                <div class="col">
+                    <div class="logo-1">
+                        <img src="../assets/img/b_logotype_2.png" alt="">
+                    </div>
+                </div>
+                <!-- / LOGO 4 -->
+
+                <!-- LOGO 5 -->
+                <div class="col">
+                    <div class="logo-1">
+                        <img src="../assets/img/b_logotype_3.png" alt="">
+                    </div>
+                </div>
+                <!-- / LOGO 5 -->
+               
+
+            </div>
+        </div>
+    </div>
 </template>
 
 
@@ -11,5 +61,10 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
+
+.container-logo{
+    height: 300px;
+    background-color: lightgrey;
+}
 
 </style>
