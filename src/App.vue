74ea445e01b637602da6d028c3318d82324e3b8e
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-09 17:40:50 +0100

    Completed: dynamic FooterBox

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,11 @@
     <summary-areas />
     <brand-logs />
     <subscribe-box />
-    <footer-box />
+    <footer-box
+    :footerContacts = "footerContacts" 
+    :topRatedProducts = "topRatedProducts"  
+    :recentPosts = "recentPosts"
+    :tagsList = "tagsList"  />
     <payment-type />
     
 
@@ -94,8 +98,156 @@ export default {
           label: "Elements",
           url: "#"
           },
-          ]
-    }}
+      ],
+      // FOOTER CONTACTS
+      footerContacts: [
+        {
+          addres: "12345 North Main Street,",
+          city: "New York, NY 55555",
+          phoneNumber: "1.800.555.6789",
+          mail: "info@compani.com",
+          web: "Theme-fusion.com",
+        }
+      ],
+      // TOP RATED PRODUCTS
+      topRatedProducts: [
+        {
+          article: "Leather Gloves",
+          image: "https://thumbs2.imgbox.com/ad/dd/IVFGCvIi_t.jpg",
+          price: "$45",
+        },
+
+        {
+          article: "Black Leather Jacket",
+          image: "https://thumbs2.imgbox.com/5b/03/QGJ1tzQo_t.jpg",
+          price: "$235",
+        },
+
+        {
+          article: "Spring Printed Dress",
+          image: "https://thumbs2.imgbox.com/db/7c/jf9rkkfH_t.jpg",
+          price: "$47",
+        }
+      ],
+      // RECENT POSTS
+      recentPosts: [
+        {
+          id: "1",
+          post: "Aenean lobortis sapien enim viverra",
+        },
+        {
+          id: "2",
+          post: "Duis ac massa semper maximus",
+        },
+        {
+          id: "3",
+          post: "Nunc fermint nulla eu justo sem id",
+        },
+        {
+          id: "4",
+          post: "Aliquam posuere magna eget nibh",
+        },
+        {
+          id: "5",
+          post: "Cras ac nulla ac consecte rutrum",
+        },
+        {
+          id: "6",
+          post: "Fusce mattis nunc ut aliquam",
+        },
+      ],
+      // TAGS
+      tagsList: [
+        {
+          id: "1",
+          tag: "Black"
+        },
+        {
+          id: "2",
+          tag: "boots"
+        },
+        {
+          id: "3",
+          tag: "Brown"
+        },
+        {
+          id: "4",
+          tag: "Casual"
+        },
+        {
+          id: "5",
+          tag: "D&G"
+        },
+        {
+          id: "6",
+          tag: "Fabric"
+        },
+        {
+          id: "7",
+          tag: "flowers"
+        },
+        {
+          id: "8",
+          tag: "Grey"
+        },
+        {
+          id: "9",
+          tag: "hat"
+        },
+        {
+          id: "1o",
+          tag: "Hipster"
+        },
+        {
+          id: "11",
+          tag: "lines"
+        },
+        {
+          id: "12",
+          tag: "multi-purpose"
+        },
+        {
+          id: "13",
+          tag: "New York"
+        },
+        {
+          id: "14",
+          tag: "Outdoors"
+        },
+        {
+          id: "15",
+          tag: "red"
+        },
+        {
+          id: "16",
+          tag: "responsive"
+        },
+        {
+          id: "17",
+          tag: "summer"
+        },
+        {
+          id: "18",
+          tag: "sweater"
+        },
+        {
+          id: "19",
+          tag: "Travel"
+        },
+        {
+          id: "20",
+          tag: "Warm"
+        },
+        {
+          id: "21",
+          tag: "White"
+        },
+        {
+          id: "22",
+          tag: "winter"
+        },
+      ],
+  }}
 }
 </script>
 
--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -1,10 +1,113 @@
 <template>
-    <div></div>
+    <div class="container-fluid container-footer">
+        <div class="container">
+            <div class="row">
+                
+                <!-- CONTACTS -->
+                <div class="col-3">
+                    <div class="logo-box mt-5">
+                        <img class="logo" src="../assets/img/classic_shop_logo_footer.png" alt="">
+                    </div>
+
+                    <div class="list-container mt-5">
+                        <ul class="list-unstyled" v-for="item in footerContacts" :key="item" >
+                            <li><a class="text-secondary text-decoration-none"> {{item.addres}} </a></li>
+                            <li><a class="text-secondary text-decoration-none"> {{item.city}} </a></li>
+                            <li><a class="text-secondary text-decoration-none"> Phone: {{item.phoneNumber}} </a></li>
+                            <li><a class="text-secondary text-decoration-none"> Email: {{item.mail}} </a></li>
+                            <li><a class="text-secondary text-decoration-none"> Web: {{item.web}} </a></li>
+                        </ul>
+                    </div>
+                    
+                    <div class="d-flex text-light pt-3">
+                    
+                        <i class="fab fa-facebook-f mx-2 "></i>
+                        <i class="fab fa-twitter mx-2"></i>
+                        <i class="fab fa-instagram mx-2"></i>
+                        <i class="fab fa-youtube mx-2"></i>
+                    
+                    </div>
+
+                </div>
+                <!-- / CONTACTS -->
+
+                <!-- TOP RATED PRODUCTS -->
+                <div class="col-3">
+
+                    <p class="text-uppercase text-muted mt-5">top rated products</p>
+
+                    <div class="list-container mt-5">
+                        <div class="list-unstyled" v-for="item in topRatedProducts" :key="item" >
+                            
+                            <img :src="item.image">
+                            
+                            <li>
+                                <a class="text-secondary text-decoration-none"> {{item.article}} </a>
+                            </li>
+
+                            <li>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </li>
+                            
+                            <li>
+                                <a class="text-secondary text-decoration-none"> {{item.price}} </a>
+                            </li>
+                            
+                            <hr class="text-muted">
+                        
+                        </div>
+                    </div>
+                </div>
+                <!-- / TOP RATED PRODUCTS -->
+                
+                
+                <!-- RECENT POSTS -->
+                <div class="col-3">
+
+                    <p class="text-uppercase text-muted mt-5">recents posts</p>
+                    
+                    <div class="list-container mt-5">
+                        <ul class="list-unstyled" v-for="item in recentPosts" :key="item" >
+                            <li>
+                                <i class="fas fa-chevron-right text-light me-2"></i>
+                                <a class="text-secondary text-decoration-none"> {{item.post}} </a>
+                                <hr class="text-muted">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- / RECENT POSTS -->
+
+
+                <!-- TAGS -->
+                <div class="col-3 column-tags ">
+                    
+                    <p class="text-uppercase text-muted mt-5">TAGS</p>
+
+                    <div class="container-tag d-inline-flex text-light fs-6 mx-1 my-1 px-2" v-for="item in tagsList" :key="item">
+                        {{item.tag}} 
+                    </div>
+                </div>
+                <!-- / TAGS -->
+                
+            </div>
+        </div>
+    </div>
 </template>
 
 
 <script>
 export default {
+    props: {
+       footerContacts: Array,
+       topRatedProducts: Array,
+       recentPosts: Array,
+       tagsList: Array, 
+    }
     
 }
 </script>
@@ -12,4 +115,22 @@ export default {
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
 
+.container-footer{
+    max-height: 750px;
+    background-color: #2B2E32;
+
+    .logo-box{
+        width: 200px;
+
+        img{
+            width: 100%;
+        }
+    }
+
+    .container-tag{
+        border: 1px solid grey;
+
+    }
+
+}
 </style>
